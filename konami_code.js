447eb63c334bde49f7fce2dfeb90ea68a83e7231
Zachary Lehmann <zlehmann@gmail.com>
2019-03-28 20:20:53 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -4,8 +4,8 @@ function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
   document.addEventListener('keydown', function(e) {
-    console.log(e.which)
-    const key = e.which;
+    console.log(parseInt(e.which))
+    const key = parseInt(e.which);
     if (key === code[index]) {
       index++;
 
