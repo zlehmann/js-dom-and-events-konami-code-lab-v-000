f04d6f224a1acfe98a90be94b9657e465064eaa7
Zachary Lehmann <zlehmann@gmail.com>
2019-03-28 19:51:31 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  const main = document.getElementById('solution');
+  const main = document.getElement('body');
 
   main.addEventListener('click', function(event) {
     alert("clicked");
