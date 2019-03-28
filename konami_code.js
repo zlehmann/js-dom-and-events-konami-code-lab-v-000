09a8fd75201f864e1f7319e10b28ee75a5a34eb7
Zachary Lehmann <zlehmann@gmail.com>
2019-03-28 19:51:04 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  const main = document.getElementById('solution');
 
+  main.addEventListener('click', function(event) {
+    alert("clicked");
+  });
 }
