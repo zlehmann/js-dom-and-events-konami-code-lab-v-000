67a328300244214f7c4921f739ab8310f21d0f84
Zachary Lehmann <zlehmann@gmail.com>
2019-03-28 20:15:54 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,4 +1,4 @@
-const code = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
