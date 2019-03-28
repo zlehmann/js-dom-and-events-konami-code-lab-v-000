726ff10c6ffe8f27f09d568378c7e40e19c6cf0d
Zachary Lehmann <zlehmann@gmail.com>
2019-03-28 19:52:23 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -2,8 +2,9 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
-  const main = document.getElement('body');
+  const main = document.getElementById('solution');
 
+  main.style.height = "200px";
   main.addEventListener('click', function(event) {
     alert("clicked");
   });
